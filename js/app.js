6d9da1886329e70db1f1b55a7c23c4861d02310b
jin-spec-0011 <jinse.kim1991@gmail.com>
2026-08-21 13:18:43 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -296,15 +296,16 @@ window.App = Object.assign(window.App || {}, {
     this.badge.refresh();
 
     // 6. Firebase 초기화
-    const firebaseConfig = {
-      apiKey: "YOUR_API_KEY",
-      authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-      databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-      projectId: "YOUR_PROJECT_ID",
-      storageBucket: "YOUR_PROJECT_ID.appspot.com",
-      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-      appId: "YOUR_APP_ID"
-    };
+  const firebaseConfig = {
+  apiKey: "AIzaSyBGYhPPlYfPnnEnqa--Sl_OYDw8VmX1fus",
+  authDomain: "gogo-manager-f0a68.firebaseapp.com",
+  databaseURL: "https://gogo-manager-f0a68-default-rtdb.firebaseio.com",
+  projectId: "gogo-manager-f0a68",
+  storageBucket: "gogo-manager-f0a68.firebasestorage.app",
+  messagingSenderId: "1016084163074",
+  appId: "1:1016084163074:web:836b8517d023638e12551b"
+};
+
 
     try {
       if (firebaseConfig.apiKey && firebaseConfig.apiKey !== "YOUR_API_KEY") {
